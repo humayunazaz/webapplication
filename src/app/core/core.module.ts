3cfc9122af8a5bb2581e398a3f7d026246dfbdb7
humayunazaz <humayun.azaz@gmail.com>
2023-11-30 17:41:26 +0500

    unused core routing is removed

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,34 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-import { CoreRoutingModule } from './core-routing.module';
 import { NavbarComponent } from './navbar/navbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { FootercpyrtsComponent } from './footercpyrts/footercpyrts.component';
 
-import {MatMenuModule} from '@angular/material/menu';
-import {MatButtonModule} from '@angular/material/button';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
-import {MatBadgeModule} from '@angular/material/badge';
-
+import { MatBadgeModule } from '@angular/material/badge';
 
 @NgModule({
-  declarations: [
-    NavbarComponent,
-    FootercpyrtsComponent
-  ],
+  declarations: [NavbarComponent, FootercpyrtsComponent],
   imports: [
     CommonModule,
-    CoreRoutingModule,
     MatToolbarModule,
     MatMenuModule,
     MatButtonModule,
     MatIconModule,
-    MatBadgeModule
+    MatBadgeModule,
   ],
-  exports: [ 
-    NavbarComponent,
-    FootercpyrtsComponent 
-  ]
+  exports: [NavbarComponent, FootercpyrtsComponent],
 })
-export class CoreModule { }
+export class CoreModule {}
